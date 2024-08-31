44669ab4def515e6f9a2e89ffb3727e6b5b0fb45
MrAnri29 <94171204+MrAnri29@users.noreply.github.com>
2024-09-01 00:03:26 +0400

    style: fix mobile issues

--- src/components/navbar/mobile/link.tsx
+++ src/components/navbar/mobile/link.tsx
@@ -9,17 +9,20 @@ interface Props {
 
 export default function LinkIndexedAnimation({ link, index }: Props) {
   return (
-    <Link href={link.href} className="group text-background/60 flex gap-x-6">
-      <div className="relative flex size-fit overflow-hidden *:duration-300 *:ease-smooth">
+    <Link
+      href={link.href}
+      className="group text-background/60 flex gap-x-2 sm:gap-x-6"
+    >
+      <div className="relative flex size-fit overflow-hidden max-sm:-mt-1 *:duration-300 *:ease-smooth">
         <ArrowRight
           size={32}
           className="absolute -left-[1rem] top-1/2 -translate-y-1/2 -translate-x-full group-hover:translate-x-full"
         />
-        <p className="text-2xl w-11 group-hover:translate-x-full">
+        <p className="text-xl sm:text-2xl w-11 group-hover:translate-x-full">
           [0{index + 1}]
         </p>
       </div>
-      <LinkAnimation className="before:-top-2 after:-top-2 font-bold text-7xl">
+      <LinkAnimation className="before:-top-2 after:-top-2 font-bold text-5xl sm:text-7xl">
         {link.name}
       </LinkAnimation>
     </Link>
