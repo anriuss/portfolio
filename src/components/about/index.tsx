34faa51dda639ce570a5ddd4fbf987a82060722a
MrAnri29 <94171204+MrAnri29@users.noreply.github.com>
2024-09-02 00:50:06 +0400

    feat: animate every text

--- src/components/about/index.tsx
+++ src/components/about/index.tsx
@@ -1,7 +1,13 @@
+"use client";
+
+import { motion } from "framer-motion";
 import { ArrowRight } from "lucide-react";
 import Link from "next/link";
+import { smooth } from "~/lib/constants/cubic-bezier";
 import { social_links } from "~/lib/constants/links";
+import { fadeInVariant } from "~/lib/constants/variants";
 import LinkAnimation from "../animations/animation-link";
+import AnimateParagraph from "../animations/animation-paragraph";
 
 export default function About() {
   return (
@@ -10,8 +16,10 @@ export default function About() {
       className="px-4 md:px-8 py-16 container min-h-[100dvh] flex flex-col justify-center rounded-xl bg-foreground text-background"
     >
       <p className="block text-4xl md:text-6xl">
-        I emphasize the importance of user experience in my work, aiming to
-        create projects that are intuitive and responsive to user needs.
+        <AnimateParagraph>
+          I emphasize the importance of user experience in my work, aiming to
+          create projects that are intuitive and responsive to user needs.
+        </AnimateParagraph>
       </p>
       <hr className="border w-full mt-16 my-8" />
       <div
@@ -19,8 +27,19 @@ export default function About() {
         className="text-2xl flex max-lg:flex-col justify-between gap-12"
       >
         <div>
-          <p className="italic text-xl">Here are my socials 👋🏼</p>
-          <div className="flex flex-col gap-y-2 mt-12">
+          <p className="italic text-xl">
+            <AnimateParagraph all={true} delay={0.5}>
+              Here are my socials 👋🏼
+            </AnimateParagraph>
+          </p>
+          <motion.div
+            className="flex flex-col gap-y-2 mt-12"
+            variants={fadeInVariant}
+            initial="initial"
+            whileInView="animate"
+            transition={{ duration: 0.5, ease: smooth, delay: 1 }}
+            viewport={{ once: true }}
+          >
             {social_links.map((link, index) => (
               <Link
                 key={index}
@@ -39,20 +58,24 @@ export default function About() {
                 <LinkAnimation>{link.name}</LinkAnimation>
               </Link>
             ))}
-          </div>
+          </motion.div>
         </div>
         <p className="lg:max-w-3xl">
-          I&apos;m a 17-year-old enthusiastic full-stack web developer dedicated
-          to turning ideas into creative solutions. I create dynamic and
-          responsive web applications using Next.js, Tailwind CSS, and other
-          modern technologies.
+          <AnimateParagraph delay={1.2} line>
+            I&apos;m a 17-year-old enthusiastic full-stack web developer &n
+            dedicated to turning ideas into creative solutions. I create dynamic
+            &n and responsive web applications using Next.js, Tailwind CSS, and
+            &n other modern technologies.
+          </AnimateParagraph>
           <br />
           <br />
-          I&apos;m involved in every step of the process: from discovery and
-          development to deployment. I focus on delivering high-quality,
-          scalable results that stimulate positive user interactions. While I
-          prioritize development, I also recognize the vital role of design,
-          particularly UI/UX, in achieving effective solutions.
+          <AnimateParagraph delay={1.5} line>
+            I&apos;m involved in every step of the process: from discovery and
+            &n development to deployment. I focus on delivering high-quality, &n
+            scalable results that stimulate positive user interactions. While I
+            &n prioritize development, I also recognize the vital role of
+            design, &n particularly UI/UX, in achieving effective solutions.
+          </AnimateParagraph>
         </p>
       </div>
     </section>
