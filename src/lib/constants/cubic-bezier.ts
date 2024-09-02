e5a80a04d49b1dfb0ae69aa0875116fba805788f
MrAnri29 <94171204+MrAnri29@users.noreply.github.com>
2024-09-02 14:37:24 +0400

    feat: add smooth redirect scroll to element within the page

--- src/lib/constants/cubic-bezier.ts
+++ src/lib/constants/cubic-bezier.ts
@@ -1,3 +1,39 @@
 import { cubicBezier } from "framer-motion";
+import type Lenis from "lenis";
 
 export const smooth = cubicBezier(0.65, 0.07, 0.015, 1);
+
+function lenisSmooth(t: number) {
+  const y1 = 0.05;
+  const y2 = 1;
+
+  const cy = 3 * y1;
+  const by = 3 * (y2 - y1) - cy;
+  const ay = 1 - cy - by;
+
+  const t2 = t * t;
+  const t3 = t2 * t;
+
+  const y = ay * t3 + by * t2 + cy * t;
+
+  return y;
+}
+
+export const scrollSmooth = (
+  e: React.MouseEvent<HTMLAnchorElement>,
+  link: { name: string; href: string },
+  lenis: Lenis | undefined,
+) => {
+  e.preventDefault();
+  e.stopPropagation();
+  const targetElement = document.getElementById(link.name.toLocaleLowerCase());
+  if (targetElement) {
+    lenis?.scrollTo(`#${link.name.toLocaleLowerCase()}`, {
+      offset: targetElement.offsetTop,
+      duration: 0.7,
+      easing: (t) => lenisSmooth(t),
+    });
+  } else {
+    window.location.href = link.href;
+  }
+};
