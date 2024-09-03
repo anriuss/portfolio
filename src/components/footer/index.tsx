e89a684f584ab077ac57ce498a07f353d3d56c75
MrAnri29 <94171204+MrAnri29@users.noreply.github.com>
2024-09-03 23:47:24 +0400

    feat: add footer

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { motion } from "framer-motion";
+import Image from "next/image";
+import Link from "next/link";
+import { smooth } from "~/lib/constants/cubic-bezier";
+import { social_links } from "~/lib/constants/links";
+
+import ANRI from "~public/logo/ANRI.svg";
+import LinkAnimation from "../animations/animation-link";
+
+export default function Footer() {
+  return (
+    <footer className="">
+      <aside
+        id="contact"
+        className="px-4 md:px-8 text-2xl flex max-sm:flex-col sm:items-end justify-between"
+      >
+        <div className="flex flex-col">
+          <span className="opacity-60">Say hi:</span>
+          <Link href="mailto:mr.anriko29@gmail.com" className="group">
+            <LinkAnimation>mr.anriko29@gmail.com</LinkAnimation>
+          </Link>
+        </div>
+        <div className="flex gap-2">
+          {social_links.slice(0, 2).map((link, index) => (
+            <Link key={index} href={link.href} className="group">
+              <LinkAnimation>{link.name}</LinkAnimation>
+            </Link>
+          ))}
+        </div>
+      </aside>
+      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((x) => {
+        return (
+          <motion.hr
+            key={x}
+            initial={{ width: 0 }}
+            whileInView={{ width: "100%" }}
+            transition={{ duration: 1, ease: smooth, delay: 0.15 * x }}
+            viewport={{ once: true }}
+            className="border my-3"
+          />
+        );
+      })}
+      <Image
+        src={ANRI}
+        alt="ANRI"
+        className="w-full p-4 mix-blend-difference"
+      />
+    </footer>
+  );
+}
